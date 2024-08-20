b684c2062e31efd9c24aa6bf4dbb53ebb79a6e2f
Victor Olabode <victonytechsolution@gmail.com>
2024-08-20 13:45:37 -0700

    adding function to the file

---
+++ function5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void calculateSubtract() {
+	int x = 83;
+	int y = 20;
+	int subtract = (x-y);
+	printf("The subtraction of x-y is: %d", subtract);
+}
+
+int main(){
+	calculateSubtract();
+	return 0;
+}
