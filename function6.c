777a8720e59709fea5bf5b1607687dd5328302ac
Victor Olabode <victonytechsolution@gmail.com>
2024-08-20 13:47:32 -0700

    adding another fuction

---
+++ function6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void calculateDivide() {
+	int x = 32;
+	int y = 8;
+	int divide = (x/y);
+	printf("The division of x/y is: %d", divide);
+}
+
+int main(){
+	calculateDivide();
+	return 0;
+}
